6620e571afc03ba84c6fe7c15583548f42e30192
ormesino <pedromateus18@hotmail.com>
2023-03-20 19:26:01 -0300

    front: adding some user login and logout logic

--- front-end/react/onlyJS/scripts/main.js
+++ front-end/react/onlyJS/scripts/main.js
@@ -1,11 +1,34 @@
+if (typeof localStorage.nome === "undefined") {
+  document.getElementById('area').innerHTML = "Olá, seja bem vindo ...!";
+} else {
+  document.getElementById('area').innerHTML = "Olá, seja bem vindo " + localStorage.nome + "!";
+  document.getElementById('btn').setAttribute('onclick', 'sair()');
+  document.getElementById('btn').innerHTML = "Sair";
+}
+
 function entrar() {
   let area = document.getElementById('area');
-  let name = prompt("Qual é o seu nome?");
-  
-  if (name === null || name === "") {
+  localStorage.setItem('nome', prompt("Qual é o seu nome?"));
+
+  if (localStorage.nome === null || localStorage.nome === "") {
     alert("Digite seu nome novamente!");
     area.innerHTML = "Olá, seja bem vindo ...!";
   } else {
-    area.innerHTML = "Olá, seja bem vindo " + name + "!";
+    area.innerHTML = "Olá, seja bem vindo " + localStorage.nome + "!";
+    document.getElementById('btn').setAttribute('onclick', 'sair()');
+    document.getElementById('btn').innerHTML = "Sair";
+  }
+}
+
+function sair() {
+  let option = prompt("Deseja realmente sair? (S/N)");
+
+  if (option === "S") {
+    localStorage.removeItem('nome');
+    document.getElementById('area').innerHTML = "Olá, seja bem vindo ...!";
+    document.getElementById('btn').setAttribute('onclick', 'entrar()');
+    document.getElementById('btn').innerHTML = "Entrar";
+  } else {
+    alert("Você não saiu!");
   }
 }
