b420654ce928dc0cc1d7d3a60cf2a7a900904560
ormesino <pedromateus18@hotmail.com>
2023-03-24 21:05:23 -0300

    front: updated the style of the project, in addition to rewrite the clear form function

--- front-end/raw/projects/interactiveForm/scripts/main.js
+++ front-end/raw/projects/interactiveForm/scripts/main.js
@@ -1,31 +1,13 @@
-function limparForm() {
+function cleanForm() {
   let inputs = document.querySelectorAll("input");
-
-  inputs.forEach((input) => input.type === 'radio' ? input.checked = false : input.value = '');
-  document.querySelector('select').value = "";
-}
-
-function enviarDados() {
-  let inputs = document.querySelectorAll("input");
-  let dados = [];
 
   inputs.forEach((input) => {
     if (input.type === 'radio') {
-      if (input.checked) {
-        dados.push(input.value);
-      }
+      input.checked = false;
     } else {
-      dados.push(input.value);
+      input.value = '';
+      input.type = 'text';
     }
   });
-
-  dados.push(document.querySelector('select').value);
-
-  db.transaction(function (tx) {
-    tx.executeSql('INSERT INTO dados (nome, email, cpf, endereco, cidade, estado, casa, resumo, cargo, descricao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', dados, function (tx, results) {
-      console.log('Dados inseridos com sucesso!');
-    }, function (tx, error) {
-      console.log('Erro ao inserir dados: ' + error.message);
-    });
-  });
+  document.querySelector('select').value = "";
 }
