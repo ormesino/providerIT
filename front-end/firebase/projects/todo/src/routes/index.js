65e851ca38d64fcd26592c02dabdb852470ee8be
ormesino <pedromateus18@hotmail.com>
2023-04-26 18:45:21 -0300

    front/firebase: changing routes for users already logged in

--- front-end/firebase/projects/todo/src/routes/index.js
+++ front-end/firebase/projects/todo/src/routes/index.js
@@ -5,12 +5,14 @@ import Home from '../pages/Home';
 import Register from '../pages/Register';
 import NotFound from '../pages/NotFound';
 
+import Private from './Private';
+
 export default function RoutesApp() {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin" element={ <Private><Admin /></Private>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
