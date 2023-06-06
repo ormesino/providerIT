e697216602589368be760c0c1e5855a7ba666684
ormesino <pedromateus18@hotmail.com>
2023-06-06 17:41:58 -0300

    back/spring: setup a test project to practice my knowledge on java + spring

---
+++ back-end/spring/learning/src/main/java/br/com/providerit/Config.java
@@ -0,0 +1,12 @@
+package br.com.providerit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+  @Bean(name = "applicationName")
+  public String applicationName() {
+    return "Sistema de Vendas";
+  }
+}
---
+++ back-end/spring/learning/src/main/java/br/com/providerit/VendasApplication.java
@@ -0,0 +1,25 @@
+package br.com.providerit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+  @Autowired
+  @Qualifier("applicationName")
+  private String applicationName;
+
+  @GetMapping("/")
+  public String app() {
+    return applicationName;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(VendasApplication.class, args);
+  }
+}
